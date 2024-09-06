72a9dcb41180f171272b24e9c64901854dea53ea
Florian Schaefer <168949185+RyTiXz@users.noreply.github.com>
2024-09-06 10:47:53 +0200

    Finish game running in console. Still need to improve spaghetti code

--- script.js
+++ script.js
@@ -38,28 +38,28 @@ const Game = (function() {
 
     // Function to play one round
     function playRound() {
-        console.log({
-            RoundCount: roundCount.getRoundCount(),
-        });
-        const input = Number(prompt('choose 1-9'));
+        // Get user input
+        let input;
+        do {
+            input =  Number(prompt('choose 1-9'))
+            if (isNaN(input) || isNaN('') || input > 9 || input < 1) {
+                alert('Wrong input. Please choose a number between 1-9')
+            } else if (!(gameBoard.includes(input))) {
+                alert('already chosen, please choose again')
+            } 
+        } while (!(gameBoard.includes(input)))
+        
+        // Replace user choice in game array
         const index = gameBoard.indexOf(input)
         if (roundCount.getRoundCount() % 2 !== 0) {
             if (gameBoard.includes(input)) {
                 gameBoard[index] = 'X';
-            } else {
-                alert('already choosen, start round from start');
-                // playRound()
-            }
+            } 
         }  else if (roundCount.getRoundCount() % 2 === 0) {
             if (gameBoard.includes(input)) {
                 gameBoard[index] = 'O';
-            } else {
-                alert('already choosen, start round from start');
-                // playRound()
-            }
+            } 
         }
-        console.log(gameBoard);
-        roundCount.increaseRoundCount();
     }
     
 
@@ -80,39 +80,49 @@ const Game = (function() {
             if (gameBoard[combination[0]] === 'X' &&
                 gameBoard[combination[1]] === 'X' &&
                 gameBoard[combination[2]] === 'X') {
-              console.log('Player 1 wins!');
+              alert('Player 1 wins!');
               return true;
             } else if (gameBoard[combination[0]] === 'O' &&
                        gameBoard[combination[1]] === 'O' &&
                        gameBoard[combination[2]] === 'O') {
-              console.log('Player 2 wins!');
+              alert('Player 2 wins!');
               return true;
-            }
+            } 
           }
     }
 
     // Function to play nine rounds or till one wins
     function playGame() {
         while (roundCount.getRoundCount() <= 5) {
+            console.log({
+                RoundCount: roundCount.getRoundCount(),
+            });
             playRound()
             if (checkForWin() === true) {
                 break
             } 
-        }
-        if (checkForWin() !== true) {
-            console.log('Tie!')
+            if (roundCount.getRoundCount() === 5) {
+                alert('Tie!')
+            }
+            roundCount.increaseRoundCount();
+            console.log(gameBoard);
         }
     }
-    playGame();
+
+    // playGame();
 
 })();
 
 /* 
 TO DO
-- restart playRound() not working correctly for now
-- 
+- Split playRound() into two factory functions
+- work on refactoring spaghetti code. Learn how to do this
+- Better understand how to write good functions
+
 
 DONE
 - build better function for check win function (don't repeat!)
+- restart playRound() not working correctly for now
+- playGame() giving two winner alerts, but Tie working correctly
 
 */
